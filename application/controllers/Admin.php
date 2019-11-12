ab9ecf3e1a210d431bc17f3769af788966ca54a2
sgcc <e32wsaq1@gmail.com>
2019-11-12 13:17:43 -0600

    titulo
    union 11 hoy

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -66,61 +66,18 @@ class Admin extends CI_Controller
         );
         $mod= $this->model_user->update_user($fields);
 
-        /*correo*/
+        $this->load->library('email');
+        $this->email->from('siconu@novauniversitas.edu.mx', 'SICONU:Credenciales');  //de paerte de
+        $this->email->to($correo);                   //para quien
+        //$this->email->cc('another@another-example.com');
+        //$this->email->bcc('them@their-example.com');
+        $this->email->subject('Usuario del sistema SICONU');
+        $this->email->message("Su usuario de ingreso es: <b>{$correo}</b> y su contraseña es <b>{$password}</b>");
+        
+        $this->email->send();
 
-        /* If you installed PHPMailer without Composer do this instead: */
-        /*
-        require 'C:\PHPMailer\src\Exception.php';
-        require 'C:\PHPMailer\src\PHPMailer.php';
-        require 'C:\PHPMailer\src\SMTP.php';
-        */
-
-        require '/usr/share/php/libphp-phpmailer/class.phpmailer.php';
-        require '/usr/share/php/libphp-phpmailer/class.smtp.php';
-        //require '/usr/share/php/libphp-phpmailer/PHPMailerAutoload.php';
-
-        /* Create a new PHPMailer object. Passing TRUE to the constructor enables exceptions. */
-        $mail = new PHPMailer(TRUE);
-
-        /* Open the try/catch block. */
-        try {
-
-          /*configuraciones adicionales*/
-          $mail->IsSMTP();
-
-          //$mail->SMTPDebug = 2;
-          //$mail->Debugoutput = 'html';
-
-          $mail->SMTPSecure = 'tls';
-          $mail->Host = 'laahui.novauniversitas.edu.mx';
-          $mail->Port = 587;
-          
-          //or more succinctly:
-          //$mail->Host = 'tls://smtp.gmail.com:587';
-
-          $mail->SMTPAuth = true;
-
-          $mail->Username = "siconu@novauniversitas.edu.mx"; // My gmail username
-          $mail->Password = 'sMg8wXLEGg6'; // My Gmail Password
-
-
-          /* Set the mail sender. */
-          $mail->setFrom('siconu@novauniversitas.edu.mx', 'SICONU:Credenciales');
-
-          /* Add a recipient. */
-          $mail->addAddress($correo, 'Profesor');
-
-          /* Set the subject. */
-          $mail->Subject = 'Usuario del sistema SICONU';
-
-          /* Set the mail message body. */
-          $mail->IsHTML(true); // Enviar como HTML
-          $mail->Body = '<font size=4>'.'Su usuario de ingreso es: <b>'.$correo.'</b> y su contraseña es: <b>profesor'.$num.'</b></font>';
-          $mail->AltBody = 'Su usuario de ingreso es: '.$correo.' y su contraseña es: profesor'.$num;
-
-          /* Finally send the mail. */
-          //$mail->send();
-        if($mail->send() and $mod==true)
+        
+        if($mod==true)
         {
           $msg_correo='<div class="alert alert-success text-center">Correo enviado</div>';
         }
@@ -137,25 +94,6 @@ class Admin extends CI_Controller
           $this->session->set_flashdata('msg', $msg_correo.'<div class="text-center alert alert-danger">Error contraseña no editada</div');
         }
         redirect('admin');
-           
-
-        }
-        catch (Exception $e)
-        {
-          /* PHPMailer exception. */
-          //echo $e->errorMessage();
-          /*Check you have the openssl extension*/
-          //echo (extension_loaded('openssl')?'SSL loaded':'SSL not loaded')."\n";
-          echo '<a class="btn btn-outline-primary my-2 my-sm-0" href="<?php echo base_url() ?>professor"> Volver </a>';
-        }
-        catch (\Exception $e)
-        {
-          /* PHP exception (note the backslash to select the global namespace Exception class). */
-          //echo $e->getMessage();
-          /*Check you have the openssl extension*/
-          //echo (extension_loaded('openssl')?'SSL loaded':'SSL not loaded')."\n";
-          echo '<a class="btn btn-outline-primary my-2 my-sm-0" href="<?php echo base_url() ?>professor"> Volver </a>';           
-        }
 
         /*correo*/
       }
@@ -165,7 +103,8 @@ class Admin extends CI_Controller
       }    
     }   
   }
-    
+
+
   public function add_professor()
   {
     $this->form_validation->set_rules('correo', 'Correo electrónico','valid_email|required|min_length[8]|max_length[50]|is_unique[usuario.matricula]');
@@ -177,86 +116,40 @@ class Admin extends CI_Controller
     $this->form_validation->set_message('is_unique', '%s ya esta registrado');
     //personalizacion de delimitadores
     $this->form_validation->set_error_delimiters('<div class="alert alert-danger text-center">', '</div>');
-	  if($this->form_validation->run() == FAlSE)
-	  {
-	  	$data['title']="Alta profesores";
-	  	$this->load->view('head',$data);
-	    $this->load->view('navbar');
-	    $this->load->view("admin/view_add_professor");
-	    $this->load->view("foot");
-	  }
-	  else
-	  {
-	    if($this->input->post("submit"))
-	    {
+    if($this->form_validation->run() == FAlSE)
+    {
+      $data['title']="Alta profesores";
+      $this->load->view('head',$data);
+      $this->load->view('navbar');
+      $this->load->view("admin/view_add_professor");
+      $this->load->view("foot");
+    }
+    else
+    {
+      if($this->input->post("submit"))
+      {
 
         $correo = $this->input->post("correo");
         $num = rand(100,999);
-	    	$fields = array(
-	    		'rol' => 2,
-	    		'nombre' =>  'profesor', 
+        $fields = array(
+          'rol' => 2,
+          'nombre' =>  'profesor', 
           'apellido_paterno' =>  'profesor',
           'apellido_materno' =>  'profesor',
           'matricula' => $correo,
           'contrasenia' =>  md5('profesor'.$num)
-	      );
+        );
         $add = $this->model_user->insert_user($fields);
-
-        /*correo*/
-
-        /* If you installed PHPMailer without Composer do this instead: */
-        /*
-        require 'C:\PHPMailer\src\Exception.php';
-        require 'C:\PHPMailer\src\PHPMailer.php';
-        require 'C:\PHPMailer\src\SMTP.php';
-        */
-
-        require '/usr/share/php/libphp-phpmailer/class.phpmailer.php';
-        require '/usr/share/php/libphp-phpmailer/class.smtp.php';
-        //require '/usr/share/php/libphp-phpmailer/PHPMailerAutoload.php';
-
-        /* Create a new PHPMailer object. Passing TRUE to the constructor enables exceptions. */
-        $mail = new PHPMailer(TRUE);
-
-        /* Open the try/catch block. */
-        try {
-
-          /*configuraciones adicionales*/
-          $mail->IsSMTP();
-
-          //$mail->SMTPDebug = 2;
-          //$mail->Debugoutput = 'html';
-
-          $mail->SMTPSecure = 'tls';
-          $mail->Host = 'laahui.novauniversitas.edu.mx';
-          $mail->Port = 587;
-          
-          //or more succinctly:
-          //$mail->Host = 'tls://smtp.gmail.com:587';
-
-          $mail->SMTPAuth = true;
-
-          $mail->Username = "siconu@novauniversitas.edu.mx"; // My gmail username
-          $mail->Password = 'sMg8wXLEGg6'; // My Gmail Password
-
-
-          /* Set the mail sender. */
-          $mail->setFrom('siconu@novauniversitas.edu.mx', 'SICONU:Credenciales');
-
-          /* Add a recipient. */
-          $mail->addAddress($correo, 'Profesor');
-
-          /* Set the subject. */
-          $mail->Subject = 'Usuario del sistema SICONU';
-
-          /* Set the mail message body. */
-          $mail->IsHTML(true); // Enviar como HTML
-          $mail->Body = '<font size=4>'.'Su usuario de ingreso es: <b>'.$correo.'</b> y su contraseña es: <b>profesor'.$num.'</b></font>';
-          $mail->AltBody = 'Su usuario de ingreso es: '.$correo.' y su contraseña es: profesor'.$num;
-
-          /* Finally send the mail. */
-          //$mail->send();
-        if($mail->send() and $add==true)
+        $this->load->library('email');
+        $this->email->from('siconu@novauniversitas.edu.mx', 'SICONU:Credenciales');  //de paerte de
+        $this->email->to($correo);                   //para quien
+        //$this->email->cc('another@another-example.com');
+        //$this->email->bcc('them@their-example.com');
+        $this->email->subject('Usuario del sistema SICONU');
+        $this->email->message("Su usuario de ingreso es: <b>{$correo}</b> y su contraseña es <b>profesor{$num}</b>");
+        
+        $this->email->send();
+        if($add==true)
         {
           $msg_correo='<div class="alert alert-success text-center">Correo enviado</div>';
         }
@@ -274,33 +167,14 @@ class Admin extends CI_Controller
           $this->session->set_flashdata('msg', $msg_correo.'<div class="text-center alert alert-danger text-center">Error profesor no añadido</div>');
         }
         redirect('admin');
-           
-
-        }
-        catch (Exception $e)
-        {
-           /* PHPMailer exception. */
-           echo $e->errorMessage();
-          /*Check you have the openssl extension*/
-          //echo (extension_loaded('openssl')?'SSL loaded':'SSL not loaded')."\n";
-          echo '<a class="btn btn-outline-primary my-2 my-sm-0" href="<?php echo base_url() ?>professor"> Volver </a>';
-        }
-        catch (\Exception $e)
-        {
-           /* PHP exception (note the backslash to select the global namespace Exception class). */
-           echo $e->getMessage();
-           /*Check you have the openssl extension*/
-           //echo (extension_loaded('openssl')?'SSL loaded':'SSL not loaded')."\n";
-          echo '<a class="btn btn-outline-primary my-2 my-sm-0" href="<?php echo base_url() ?>professor"> Volver </a>';           
-        }
 
         /*correo*/
-	    }
-	    else
-	    {
-	    	redirect('admin');
-	    }
-	  }//fin de form validation
+      }
+      else
+      {
+        redirect('admin');
+      }
+    }//fin de form validation
   }
 
   public function eliminar_professor(){
