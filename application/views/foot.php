261a4000fb191daf1ec0c0ec08a97ec2a1d42000
sgcc <e32wsaq1@gmail.com>
2019-12-03 13:26:08 -0600

    chinin gay
    sadasd

--- application/views/foot.php
+++ application/views/foot.php
@@ -18,28 +18,57 @@
 
   
   $(document).ready(function(){
-          $("form").submit(function(e){
-              var condicion = $("#rgpd").is(":checked");
-              if(!condicion){
-                  e.preventDefault();
-              }else{
-                  $("[name='enviar']").click(function(){
-                      $(this).attr("disabled","disabled");
-                  });
-              };  
-          });
-          $("input").blur(function(){
-              $("[name='enviar']").removeAttr("disabled");
-          })
-          
+
+      $("form").submit(function(e){
+          var condicion = $("#rgpd").is(":checked");
+          if(!condicion){
+              e.preventDefault();
+          }else{
+              $("[name='enviar']").click(function(){
+                  $(this).attr("disabled","disabled");
+              });
+          };  
       });
+      $("input").blur(function(){
+          $("[name='enviar']").removeAttr("disabled");
+      })
+      
+  });
 
 
   $(window).ready(function(){
 
+    habilitar();
+
     <?php if(isset($modal)) echo "$('#".$modal."').modal('show')";?>    
 
     $('table.table.table-hover.table-responsive-md.col-md-5:nth-child(2n)').addClass('offset-2');
+
+    //no editar
+
+      //echo print_r($exercises,1);
+      /*
+      [id_empresa] => 2
+      [estado] => 0
+      */
+      
+      var ejs = new Array();
+      <?php 
+        /*
+        echo $asientos[0]->id_asiento;
+        echo 'num asientos'.count($asientos);
+        */
+      ?>
+      <?php 
+      
+        if (isset($exercises)) {
+          for ($i=0; $i < count($exercises); $i++) { 
+      ?>
+        var ejs.push(<?php echo $exercises[$i]->id_empresa;?>) = <?php echo $exercises[$i]->id_empresa; ?>
+      <?php  }} ?>
+          
+
+    
 
     $("#sendcont").click(function() {
       
